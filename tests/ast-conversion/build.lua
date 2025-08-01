dc3944c3c0204f59a0bf1fb64348e8b741a83c67
q <officialaxos7@gmail.com>
2025-08-01 16:55:20 -0500

    add some more testing stuff
    [git-p4: depot-paths = "//enosi/ecs/": change = 380]

---
+++ tests/ast-conversion/build.lua
@@ -0,0 +1,27 @@
+local lake = require "lake"
+local o = lake.obj
+local List = require "iro.List"
+
+return function(
+    build_dir, 
+    lpp_params, 
+    cpp_params, 
+    link_params,
+    iro_objs)
+
+  local objs = List {}
+  objs:pushList(iro_objs)
+
+  local function compileLpp(lfile)
+    local cpp = build_dir.."/"..lfile..".cpp"
+    objs:push(
+      o.Lpp(lfile)
+        :preprocessToCpp(cpp, lpp_params)
+        :compile(cpp..".o", cpp_params))
+  end
+
+  compileLpp "tests/ast-conversion/main.lpp"
+
+  -- o.Exe(build_dir.."/tests/ast-conversion/run")
+  --   :link(objs, link_params)
+end
